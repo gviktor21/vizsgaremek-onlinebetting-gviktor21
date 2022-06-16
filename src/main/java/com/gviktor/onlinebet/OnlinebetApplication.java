9eb34bf76506524ea9db69af5ded499ee8be802b
gviktor21 <godov2@gmail.com>
2022-06-16 22:45:06 +0200

    Initial Commit.

---
+++ src/main/java/com/gviktor/onlinebet/OnlinebetApplication.java
@@ -0,0 +1,13 @@
+package com.gviktor.onlinebet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gviktor/onlinebet/OnlinebetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gviktor.onlinebet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
