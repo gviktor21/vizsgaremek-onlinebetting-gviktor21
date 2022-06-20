b2c8405a024246360074c51427dad38d8b62c535
Godó Viktor <godov2@gmail.com>
2022-06-20 17:49:21 +0200

    Add some validation to adding events. Ant it's Controller returns ResponseEntity.

--- src/main/java/com/gviktor/onlinebet/controller/EventController.java
+++ src/main/java/com/gviktor/onlinebet/controller/EventController.java
@@ -4,8 +4,12 @@ import com.gviktor.onlinebet.dto.EventCreate;
 import com.gviktor.onlinebet.dto.EventShow;
 import com.gviktor.onlinebet.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,23 +23,32 @@ public class EventController {
     }
 
     @GetMapping
-    public List<EventShow> getAllEvents(){
-        return service.getAllEvents();
+    public ResponseEntity<List<EventShow>> getAllEvents(){
+        return new ResponseEntity<>(service.getAllEvents(), HttpStatus.OK);
     }
     @GetMapping("/{id}")
-    public EventShow getEvent(@PathVariable int id){
-        return service.getEventById(id);
+    public ResponseEntity<EventShow> getEvent(@PathVariable int id) {
+        return new ResponseEntity<>(service.getEventById(id),HttpStatus.OK);
     }
     @PutMapping("/{id}")
-    public void updateEvent(@PathVariable int i,@RequestBody EventCreate eventCreate){
-        service.updateEvent(i,eventCreate);
+    public ResponseEntity<Void> updateEvent(@PathVariable int id, @RequestBody  @Valid EventCreate eventCreate, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST) ;
+        }
+        service.updateEvent(id,eventCreate);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
     @DeleteMapping("/{id}")
-    public void deleteEvent(@PathVariable int id){
+    public ResponseEntity<Void> deleteEvent(@PathVariable int id){
         service.deleteEventById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
     @PostMapping
-    public void addEvent(@RequestBody EventCreate eventCreate){
+    public ResponseEntity<Void> addEvent(@RequestBody @Valid EventCreate eventCreate,BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         service.addEvent(eventCreate);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/gviktor/onlinebet/dto/EventCreate.java
+++ src/main/java/com/gviktor/onlinebet/dto/EventCreate.java
@@ -3,13 +3,23 @@ package com.gviktor.onlinebet.dto;
 import com.gviktor.onlinebet.model.EventType;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 public class EventCreate {
+    @NotBlank
+    @NotNull
     private EventType eventType;
+    @NotBlank
+    @NotNull
     private LocalDate startDate;
+    @NotBlank
+    @NotNull
     private LocalDate endDate;
+    @Length(max = 255,message = "Length cannot go beyond 255 characters.")
     private String description;
 }
