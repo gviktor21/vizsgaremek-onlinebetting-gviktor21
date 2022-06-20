8cd0cb76983893cb9812637737ff239ed336bc8a
Godó Viktor <godov2@gmail.com>
2022-06-20 18:13:48 +0200

    SportEvent validation added. Its Controllers now return with ResponseEntity.

--- src/main/java/com/gviktor/onlinebet/controller/SportEventController.java
+++ src/main/java/com/gviktor/onlinebet/controller/SportEventController.java
@@ -3,8 +3,12 @@ package com.gviktor.onlinebet.controller;
 import com.gviktor.onlinebet.dto.SportEventCreate;
 import com.gviktor.onlinebet.dto.SportEventShow;
 import com.gviktor.onlinebet.service.SportEventService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -17,24 +21,34 @@ public class SportEventController {
     }
 
     @GetMapping
-    public List<SportEventShow> getAllSportEvents(){
-        return service.getAllSportEvent();
+    public ResponseEntity<List<SportEventShow>> getAllSportEvents(){
+        return new ResponseEntity<>(service.getAllSportEvent(), HttpStatus.OK);
     }
     @PostMapping("/{id}")
-    public void addSportEventToEventIdentifiedByItsId(@PathVariable int id, @RequestBody SportEventCreate sportEventCreate){
+    public ResponseEntity<Void> addSportEventToEventIdentifiedByItsId(@PathVariable int id, @RequestBody @Valid SportEventCreate sportEventCreate, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         service.addSportEvent(id,sportEventCreate);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
     @GetMapping("/{id}")
-    public SportEventShow getSportEvent(@PathVariable int id){
-        return service.getSportEventById(id);
+    public ResponseEntity<SportEventShow> getSportEvent(@PathVariable int id){
+        return new ResponseEntity<>(service.getSportEventById(id),HttpStatus.OK);
     }
     @PutMapping("/{id}")
-    public void updateSportEvent(@PathVariable int id, @RequestBody SportEventCreate sportEventCreate){
+    public ResponseEntity<Void> updateSportEvent(@PathVariable int id, @RequestBody @Valid SportEventCreate sportEventCreate,BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        }
         service.updateSportEvent(id,sportEventCreate);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
     @PutMapping("/{id}")
-    public void deleteSportEvent(@PathVariable int id){
+    public ResponseEntity<Void> deleteSportEvent(@PathVariable int id){
         service.deleteSportEventById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
--- src/main/java/com/gviktor/onlinebet/dto/SportEventCreate.java
+++ src/main/java/com/gviktor/onlinebet/dto/SportEventCreate.java
@@ -1,9 +1,13 @@
 package com.gviktor.onlinebet.dto;
 
-import com.gviktor.onlinebet.model.SportParticipant;
 import com.gviktor.onlinebet.model.SportType;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class SportEventCreate {
+    @NotBlank
+    @NotNull
     private SportType sportType;
     private int winnerId;
 }
