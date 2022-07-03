151cd6a39f21fc1a2877cc323822b042e5b4e5ef
gviktor21 <godov2@gmail.com>
2022-07-03 19:35:46 +0200

    Some todo cleared.

--- src/main/java/com/gviktor/onlinebet/OnlinebetApplication.java
+++ src/main/java/com/gviktor/onlinebet/OnlinebetApplication.java
@@ -7,9 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 //Nagy megcsinalni list
-//todo: docker konténer elkészítése
-//todo: readme.md elkészítenni és postmannel az ottani endpointokat tesztelni
-//todo: futtatási parancsok hozzáadása.
 @SpringBootApplication
 public class OnlinebetApplication {
 
--- src/main/java/com/gviktor/onlinebet/model/BidAppUser.java
+++ src/main/java/com/gviktor/onlinebet/model/BidAppUser.java
@@ -10,7 +10,6 @@ import javax.persistence.OneToMany;
 import java.time.LocalDate;
 import java.util.List;
 
-//todo change some types
 @Entity
 @Data
 @NoArgsConstructor
